af725ac5a2f961d97cb79d48ed1f9a15713611f9
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-13 20:51:06 +0000

    Added some notes to make reading the code more understandable

--- main.cs
+++ main.cs
@@ -6,10 +6,11 @@ class Program {
         sum = 0;
     Console.WriteLine ("input a number");
       UserInput = int.Parse(Console.ReadLine());
-    for( int i = 1 ; i < UserInput; i++)
+    
+    for( int i = 1 ; i < UserInput; i++)// repeats until i is equal to the user input
     { 
       if (UserInput % i == 0){
-            sum = sum+i;
+            sum = sum+i; // calculates if the sum of divisors equal user input 
       }
     }
     
